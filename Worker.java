cca5e9ac1391bdb3ea8fa9b8a7227e715708e2cd
Eyuel Kahsay <eyuel.kahsay@ucalgary.ca>
2025-11-07 23:06:27 -0700

    Add base files: Worker, Server, and CriticalSection_Base from assignment template

--- Worker.java
+++ Worker.java
@@ -1,6 +1,6 @@
 public class Worker extends Thread {
     private CriticalSection_Base Section;
-    // Java threads have an ID already, however you can use this to make sure you have control over the starting value and increment
+    //java threads have an ID already, however you can use this to make sure you have control over the starting value and increment
     int ID;
 
     public Worker(CriticalSection_Base Section, int ID) {
@@ -9,16 +9,14 @@ public class Worker extends Thread {
     }
 
     public void run() {
-        // Each thread enters and exits the critical section 5 times
-        for (int i = 0; i < 5; i++) {
-            Section.EntrySection(this);
-            try {
-                Section.CriticalSection(this);
-            } catch (InterruptedException e) {
-                // Elevate the exception, this should not happen
-                throw new RuntimeException(e);
-            }
-            Section.ExitSection(this);
+        //This is for a single entry/exit of the CS
+        Section.EntrySection(this);
+        try {
+            Section.CriticalSection(this);
+        } catch (InterruptedException e) {
+            //elevate the exception, this should not happen
+            throw new RuntimeException(e);
         }
+        Section.ExitSection(this);
     }
 }
